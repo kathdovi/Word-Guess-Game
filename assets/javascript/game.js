aaf2d5279be503a4ab892216fbd76f0fac6b698f
Kathleen Doviken <kathleendoviken@gmail.com>
2018-08-16 13:40:12 -0500

    update...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,12 @@
-let possibilities = ["Breakfast*at*Tiffanys", "Up", "Forrest*Gump", "Pulp*Fiction", "The*Truman*Show", "Rent", "Casablanca", "The*Godfather", "The*Matrix", "The*Wizard*Of*Oz", "Goodfellas", "Back*To*The*Future", "A*Clockwork*Orange", "Psycho"];
+let possibilities = ["Breakfast*at*Tiffanys", "Up", "Forrest*Gump", "Pulp*Fiction", "The*Truman*Show", "Rent", "Casablanca", "The*Godfather", "The*Matrix", 
+    "The*Wizard*Of*Oz", "Goodfellas", "Back*To*The*Future", "A*Clockwork*Orange", "Psycho", "Clue", "Star*Trek", "Finding*Nemo", "Scarface", "Vertigo", 
+    "Double*Identity", "The*Shining", "Rear*Window", "Citizen*Kane", "The*Usual*Suspects", "Schindlers*List", "Star*Wars", "The*Silence*Of*The*Lambs", "Jaws"];
 let wins = 0;
 
-// starting variables
-let guessesLeft = 7;
-let lettersAlreadyGuessed = [];
-let lettersAvailable = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let currentWord = possibilities[Math.floor(Math.random() * possibilities.length)].toUpperCase();
-let gameOver = false;
-let encodedWord = encodeWord(currentWord);
+// Call reset to display starting values & start first game
+reset(); 
 
-document.querySelector(".current-phrase").innerHTML = encodedWord.split('').join(" ");
-document.querySelector(".guesses-left").innerHTML = guessesLeft;
-document.querySelector(".letters-left").innerHTML = lettersAvailable.join(" ");
-document.querySelector(".win-num").innerHTML = wins;
-
+// Reset values for each game
 function reset() {
     guessesLeft = 7;
     lettersAlreadyGuessed = [];
@@ -25,8 +18,10 @@ function reset() {
     document.querySelector(".current-phrase").innerHTML = encodedWord.split('').join(" ");
     document.querySelector(".guesses-left").innerHTML = guessesLeft;
     document.querySelector(".letters-left").innerHTML = lettersAvailable.join(" ");
+    document.querySelector(".win-num").innerHTML = wins;
 }
 
+// Change the word into all _ and *
 function encodeWord(currentWord) {
     let encodedWord = "";
     for (let i = 0; i < currentWord.length; i++) {
@@ -57,26 +52,23 @@ document.addEventListener('keyup', function (e) {
         let index = lettersAvailable.indexOf(input);
         lettersAvailable.splice(index, 1);
         document.querySelector(".letters-left").innerHTML = lettersAvailable.join(" ");
+        tempWord = encodedWord.split('').join(" ");
+        document.querySelector(".current-phrase").innerHTML = tempWord;
     }
 
-    // if input isn't in the current word , input is a letter , input is not already guessed
+    // if input isn't in the current word , input is a letter, input is not already guessed: take away a guess
+    // & modify letters left
     if ((!currentWord.includes(input)) && (key >= 65) && (key <= 90) && ($.inArray(input, lettersAlreadyGuessed) === -1)) {
         console.log($.inArray(input, lettersAlreadyGuessed));
         lettersAlreadyGuessed.push(input);
         guessesLeft--;
         document.querySelector(".guesses-left").innerHTML = guessesLeft;
-        if (guessesLeft === 0) {
-            document.querySelector(".win").innerHTML = "You lose! The phrase was: " + currentWord;
-            gameOver = true;
-        }
         let index = lettersAvailable.indexOf(input);
         lettersAvailable.splice(index, 1);
         document.querySelector(".letters-left").innerHTML = lettersAvailable.join(" ");
+        tempWord = encodedWord.split('').join(" ");
+        document.querySelector(".current-phrase").innerHTML = tempWord;
     }
-
-
-    tempWord = encodedWord.split('').join(" ");
-    document.querySelector(".current-phrase").innerHTML = tempWord;
 
     // Win Condition
     if (encodedWord === currentWord) {
@@ -84,7 +76,12 @@ document.addEventListener('keyup', function (e) {
         wins++;
         document.querySelector(".win").innerHTML = "You win!";
         document.querySelector(".win-num").innerHTML = wins;
+    }
 
+    // Lose Condition
+    if (guessesLeft === 0) {
+        document.querySelector(".win").innerHTML = "You lose! The phrase was: " + currentWord;
+        gameOver = true;
     }
 
 
